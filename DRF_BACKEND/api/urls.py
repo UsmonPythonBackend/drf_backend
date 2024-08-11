1c8f0f1ff32e97f51d1bd772830eb83616da62f9
UsmonPythonBackend <166992108+UsmonPythonBackend@users.noreply.github.com>
2024-08-11 05:46:32 +0500

    Update urls.py

--- DRF_BACKEND/api/urls.py
+++ DRF_BACKEND/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import ArtistViewSetWeb, ArtistViewSetTelegram
+from .views import ArtistViewSetWeb, ArtistViewSetTelegram, AlbomViewSetWeb, SongsViewSetWeb, AlbomViewSetTelegram, SongsViewSetTelegram
 
 router = routers.DefaultRouter()
 router.register(r'artist-web', ArtistViewSetWeb, basename='artist-web')
-
+router.register(r'albom-web', AlbomViewSetWeb, basename='albom-web')
+router.register(r'songs-web', SongsViewSetWeb, basename='songs-web')
 router.register(r'artist-telegram', ArtistViewSetTelegram, basename='artist-telegram')
+router.register(r'albom-telegram', AlbomViewSetTelegram, basename='albom-telegram')
+router.register(r'songs-telegram', SongsViewSetTelegram, basename='songs-telegram')
 
 
 
